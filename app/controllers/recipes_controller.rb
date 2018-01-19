bcfdd876672ef7a5b48fa7d22b21d883e4ff3f66
Brian Ruff <thelonewolf198927@gmail.com>
2018-01-19 16:47:50 -0500

    push

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,7 @@ class RecipesController < ApplicationController
   def index
     @recipes = if params[:term]
                  Recipe.where('name ILIKE ?', "%#{params[:term]}").page
-                 (params[:page]).per(4)
+                 (params[:page])
                else
                  Recipe.all.page(params[:page]).per(4)
                end
